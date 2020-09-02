e62aa3da81103b94dfd48bd90b9eba3ef6a1750f
Rafael Gómez <r.gomez@city-ss.com>
2020-09-02 13:35:54 +0200

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,19 @@
+<script>
+import Vue from "vue";
+import { CondioSample, CondioButton } from "@/entry";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    CondioSample,
+    CondioButton,
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <condio-sample />
+    <condio-button />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as CondioSample } from "./condio-sample.vue";
+export { default as CondioButton } from "./condio-button.vue";
